ce2c057aff05c3ee5046f64031351950cba9c3e4
jrcodd <33839969+jrcodd@users.noreply.github.com>
2017-12-26 17:08:56 -0800

    Add files via upload

--- src/ChristmasTree.java
+++ src/ChristmasTree.java
@@ -5,78 +5,96 @@
  *    
  ******************************************************************************/
 
-import org.jointheleague.graphical.robot.Robot;
+import java.awt.Color;
 
+import org.jointheleague.graphical.robot.Robot;
 
 public class ChristmasTree {
 
-	Robot rob  = new Robot();
+	Robot rob = new Robot("mini");
+
+	public static void main(String[] args) {
+
+		ChristmasTree ohChristmasTree = new ChristmasTree();
+		ohChristmasTree.setup();
+		ohChristmasTree.drawStar();
+		ohChristmasTree.drawTreeBody();
+		ohChristmasTree.drawTreeTrunk();
+		
+	}
+
+	double treeWidth = 16;
+	double scale = 1.1; // This is how much the width of the tree increases with each layer down
+
+	void drawTreeBody() {
+		rob.setSpeed(100);
+		rob.penDown();
+		rob.setPenWidth(12);
+		// 8. Change the color of the line the robot draws to forest green
+		rob.setPenColor(new Color(32, 102, 17));
+
+		// 1. Make a variable for turnAmount and set it to 175
+		int turnAmount = 175;
+
+		// 2. Start the Robot facing to the right
+		rob.setAngle(90);
+
+		// 5. Repeat steps 3 through 11, 11 times
+		for (int i = 0; i < 11; i++) {
+
+			// 3. Move the robot the width of the tree
+			rob.move((int) treeWidth);
+			// 4. Turn the robot the current turnAmount to the right
+			rob.turn(turnAmount);
+			// 6. Set the treeWidth to the current treeWidth times the scale
+			treeWidth *= scale;
+			// 7. Move the robot the width of a tree again
+			rob.move((int) treeWidth);
+			// 9. Turn the robot the current turn amount to the LEFT
+			rob.turn(-turnAmount);
+			// 10. Set the treeWidth to the current treeWidth times the scale again
+			treeWidth *= scale;
+			// 11. Decrease turnAmount by 1
+			turnAmount -= 1;
+		}
+	}
+
+	void drawTreeTrunk() {
+		// 1. Move the robot half the width of the tree
+		rob.move((int) (treeWidth / 2));
+		// 2. Change the robot so that it is pointing straight down
+		rob.setAngle(180);
+		// 4. Set the pen width to the tree width divided by 10
+		rob.setPenWidth((int) (treeWidth / 10));
+		// 5. Change the color of the line the robot draws to brown
+		rob.setPenColor(new Color(68, 51, 17));
+		// 3. Move the robot a quarter the tree width
+		rob.move((int) (treeWidth / 4));
+	}
+
+	void drawStar() {
+		// * Optional: Draw a red star on top of the tree. Hint: 144 degrees makes a
+		// star.
+		rob.setPenWidth(15);
+		rob.setPenColor(new Color(238, 255, 187));
+		rob.setSpeed(100);
+		rob.penDown();
+		rob.setAngle(20);
+		for (int i = 0; i < 5; i++) {
+			rob.move(100);
+			rob.turn(144);
+
+		}
+
+		rob.setAngle(90);
+		rob.penUp();
+		rob.move(22);
+	}
+void setup() {
+	rob.hide();
+	rob.setSpeed(1000000);
 	
-    public static void main(String[] args) {
-        ChristmasTree ohChristmasTree = new ChristmasTree();
-        ohChristmasTree.drawStar();
-        ohChristmasTree.drawTreeBody();
-        ohChristmasTree.drawTreeTrunk();
-    }
-
-
-    double treeWidth = 15;
-    double scale = 1.1;    //This is how much the width of the tree increases with each layer down
-
-
-    void drawTreeBody() {
-        // 8. Change the color of the line the robot draws to forest green    
-
-
-        // 1. Make a variable for turnAmount and set it to 175
-
-
-        // 2. Start the Robot facing to the right
-
-
-        // 5. Repeat steps 3 through 11, 11 times
-
-
-            // 3. Move the robot the width of the tree
-    
-            // 4. Turn the robot the current turnAmount to the right
-    
-            // 6. Set the treeWidth to the current treeWidth times the scale
-    
-            // 7. Move the robot the width of a tree again
-    
-            // 9. Turn the robot the current turn amount to the LEFT
-    
-            // 10. Set the treeWidth to the current treeWidth times the scale again
-    
-            // 11. Decrease turnAmount by 1
-
-
+	
 }
-    
-    void drawTreeTrunk() {
-        // 1. Move the robot half the width of the tree
-
-
-        // 2. Change the robot so that it is pointing straight down
-
-
-        // 4. Set the pen width to the tree width divided by 10
-
-
-        // 5. Change the color of the line the robot draws to brown
-
-
-        // 3. Move the robot a quarter the tree width
-
-
-    }
-    
-    void drawStar() {
-        // * Optional: Draw a red star on top of the tree. Hint: 144 degrees makes a star.
-    
-    }
-
-
 }
 
---
+++ src/HappyNewYear.java
@@ -0,0 +1,45 @@
+import java.text.BreakIterator;
+import java.util.Random;
+
+// This recipe adds random numbers until the sum is exactly 2018!!!
+//     Happy New Year!
+
+public class HappyNewYear {
+	public static void main(String[] args) {
+		// 1. Make a variable 'newYear' and initialize it to 2018.
+		int newYear = 2018;
+
+		// 7. Repeat steps 2 to 6 (10 times)
+		for (int f = 0; f < 12; f++) {
+
+			// 2. Make a variable 'sum' and initialize to 0
+			int sum = 0;
+
+			// 3. Repeat steps 4 to 6 (500 times)
+			for (int i = 0; i < 500; i++) {
+
+				// 4. Get a random number between 0 and 50
+				int r = new Random().nextInt(50);
+
+				// 5. If sum is less than newYear then add your random
+				// number to your sum, otherwise subtract it.
+				if (sum < newYear) {
+					sum = sum + r;
+
+				} else {
+					sum -= r;
+				}
+				// 6. If your sum matches newYear, then print the number of tries
+				// to the console and use the "break" statement to exit the loop
+				if (sum == newYear) {
+					System.out.println("It took you " + i + " tries to get to the new year!");
+					break;
+				}
+			}
+		}
+
+		// *** BONUS: Keep track of your best score and worst score, and print
+		// the results to the console.
+
+	}
+}
